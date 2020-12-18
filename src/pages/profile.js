9f6d0b618790281a6bee94d2c016662588f29054
sωıтcн <40813662+swyssauro@users.noreply.github.com>
2020-12-17 23:24:11 -0300

    📢 update components

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,10 +1,10 @@
-import api from "../services/axios";
+/* import api from "../services/axios";
 import { useEffect, useState } from 'react';
-import { useParams } from "react-router-dom";
+import { useParams } from "react-router-dom"; */
 
 export default function Home() {
-    
-        const [perfil, setPerfil] = useState([]);
+
+    /*     const [perfil, setPerfil] = useState([]);
         const [list, setList] = useState([]);
         const params = useParams();
     
@@ -15,19 +15,17 @@ export default function Home() {
     
             api.get(`user/${params.username}/animes`)
                 .then(response => setList(response.data))
-        }, [params.username]);
+        }, [params.username]); */
 
     return (
-        <div className="grid-container">
-            <div className="backdrop"></div>
-            <div className="leftbar"></div>
-            <div className="itens-info"><h3>{perfil.username}</h3><br/><p>{perfil.description}</p></div>
-            <div className="painel">
-                {list.map((lista, index) =>
-                <li key={index}>{lista.title}</li>
-                )}
+        <div class="grid-container">
+            <div class="left-bar"></div>
+            <div class="backdrop"></div>
+            <div class="info-bar">
+                <div class="avatar"></div>
+                <div class="meta-info"></div>
             </div>
-            <div className="avatar"></div>
+            <div class="painel"></div>
         </div>
     );
 }
