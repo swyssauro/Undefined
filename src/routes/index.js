058d63553a5d8430fc20290f61b132e539b4e7bd
sωıтcн <40813662+swyssauro@users.noreply.github.com>
2021-01-03 23:01:29 -0300

    🛎 add new route

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ import PrivateRoute from './private';
 import Home from '../pages/home';
 import Register from '../pages/register';
 import Profile from '../pages/profile';
+import Anime from '../pages/animes';
 import Welcome from '../pages/welcome';
 import Erro404 from '../pages/404';
 
@@ -14,6 +15,7 @@ export default function Routes() {
                 <Route exact path='/sign-in' component={ Home } />
                 <Route exact path='/sign-up' component={ Register } />
                 <PrivateRoute exact path='/welcome/:tmdb' component={ Welcome } />
+                <PrivateRoute exact path="/anime/:nome" component={ Anime } />
                 <PrivateRoute exact path="/:username" component={ Profile } />
                 <Route component={ Erro404 } />
             </Switch>
